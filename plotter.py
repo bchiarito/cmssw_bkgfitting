bf05a0ce5367a2f0565271cd5b84d52b02a448a0
Brandon Chiarito <bchiarito@gmail.com>
2023-06-29 19:59:44 -0400

    add bin by bin error, and add --integral option

--- plotter.py
+++ plotter.py
@@ -24,6 +24,7 @@ parser.add_argument("--ratio", default=False, action="store_true", help="create 
 parser.add_argument("--fit", default=False, action="store_true", help="create fit plots only")
 parser.add_argument("--name", default="plots", help="create name for plots pdf")
 parser.add_argument("--ftest", default=None, help="format: '<CHEB_TYPE> <MAXDEGREE>', default is no f-test")
+parser.add_argument("--integral", default=False, action="store_true", help="add I to tight fit")
 
 # parse args
 args = parser.parse_args()
@@ -551,7 +552,7 @@ for item in plots:
                         
                                 fitted_func = util.HistogramToFunction(loose_fit_as_hist)
                                 fitted_func_times_constant, _ = util.MultiplyWithPolyToTF1(fitted_func, 0, cheb=0)
-                                h_egamma_tight.Fit(fitted_func_times_constant, '0L') 
+                                h_egamma_tight.Fit(fitted_func_times_constant, '0L' if not args.integral else '0LI')
                                 tight_fit_w_constant = util.TemplateToHistogram(fitted_func_times_constant, 1000, 0, 50)
 
                                 FTEST = True if args.ftest else False
@@ -559,7 +560,7 @@ for item in plots:
                                     CHEB_TYPE = 1
                                     DEGREE = 2
                                     func_with_poly, _ = util.MultiplyWithPolyToTF1(fitted_func, DEGREE, cheb=CHEB_TYPE)
-                                    h_egamma_tight.Fit(func_with_poly, '0L') 
+                                    h_egamma_tight.Fit(func_with_poly, '0L' if not args.integral else '0LI')
                                     tight_fit_as_hist = util.TemplateToHistogram(func_with_poly, 1000, 0, 50)
 
                                 if FTEST:
@@ -571,7 +572,7 @@ for item in plots:
                                     statboxes = []
                                     for degree in range(NUM_DEGREES+1):
                                         func_with_poly, _ = util.MultiplyWithPolyToTF1(fitted_func, degree, cheb=CHEB_TYPE)
-                                        fitresult = h_egamma_tight.Fit(func_with_poly, '0SL')
+                                        fitresult = h_egamma_tight.Fit(func_with_poly, '0SL' if not args.integral else '0SLI')
                                         tight_fit_as_hist = util.TemplateToHistogram(func_with_poly, 1000, 0, 50)
 
                                         fitfuncs.append(func_with_poly)
@@ -599,6 +600,28 @@ for item in plots:
                                     tight_stat = statboxes[best_d]
 
                                 just_poly = util.ExtractPolyFromTightFit(func_with_poly, cheb=CHEB_TYPE)
+
+                                # determine bin-by-bin error
+                                TARGET_DIFFERENCE = 0.1
+                                STEP_SIZE = 0.001
+                                integral = False
+                                hist = h_egamma_tight
+                                fit = func_with_poly
+                                ndof = util.count_nonzero_bins(hist) - fit.GetNpar()
+                                rss, _ = util.RSS(fit, hist, error=0, integral=integral)
+                                chi2 = math.sqrt(rss)
+                                chi2_ndof = chi2 / ndof
+                                chi2_diff = abs(chi2_ndof - 1.0)
+                                error = 0.00
+                                while chi2_diff > TARGET_DIFFERENCE:
+                                    error += STEP_SIZE
+                                    #print('trying', error)
+                                    rss, _ = util.RSS(fit, hist, error=error, integral=integral)
+                                    chi2 = math.sqrt(rss)
+                                    chi2_ndof = chi2 / ndof
+                                    chi2_diff = abs(chi2_ndof - 1.0)
+                                bin_bin_error = error
+                                print(chi2_ndof, bin_bin_error)
 
                                 h_loose_pull_num = h_egamma_loose.Clone()
                                 h_loose_pull_num.Reset()
@@ -658,10 +681,12 @@ for item in plots:
                                 else: legend1 = ROOT.TLegend(0.62, 0.27, 0.9, 0.37)
                                 legend1.AddEntry(h_egamma_loose, "Loose Photon, " + str(h_egamma_loose.GetEntries()), "l")
                                 #if not args.ratio: legend1.AddEntry(0, "Chi2/NDF: " + str(chi2 / ndf), "")
-                                legend2 = ROOT.TLegend(0.65, 0.25, 0.9, 0.35)
+                                #legend2 = ROOT.TLegend(0.65, 0.25, 0.9, 0.45)
+                                legend2 = ROOT.TLegend(0.29, 0.70, 0.62, 0.89)
                                 legend2.AddEntry(h_egamma_tight, "Tight Photon, " + str(h_egamma_tight.GetEntries()), "l")
-                                legend2.AddEntry(tight_fit_as_hist, "Fitted Tight (degree "+str(func_with_poly.GetNpar()-1)+")", "l")
-                                legend2.AddEntry(tight_fit_w_constant, "Constant fit, C = {:.5}".format(fitted_func_times_constant.GetParameter(0)), "l")
+                                if FTEST: legend2.AddEntry(tight_fit_as_hist, "Fit w f-test (Degree "+str(func_with_poly.GetNpar()-1)+")", "l")
+                                else: legend2.AddEntry(tight_fit_as_hist, "Fit (Degree "+str(func_with_poly.GetNpar()-1)+")", "l")
+                                legend2.AddEntry(tight_fit_w_constant, "Constant fit, C = {:.4}".format(fitted_func_times_constant.GetParameter(0)), "l")
                                 
                                 # Draw plots
                                 if args.fit: c1.cd(1)
@@ -716,16 +741,8 @@ for item in plots:
                                         tight_fit_w_constant.Draw('same')
                                         h_egamma_tight.Draw("e same")
 
-                                        #polymax = 1.1*just_poly.GetMaximum()
-                                        #scale = just_poly.GetParameter(0) * (h_egamma_tight.GetMaximum()/polymax)
-                                        #just_poly.SetParameter(0, scale)
-                                        #print(polymax, just_poly.GetParameter(0), ROOT.gPad.GetUymax(), scale)
-                                        #just_poly.Draw("same")
-
                                         legend2.Draw("same")
 
-                                        #c1.cd()
-                                        #overlay = ROOT.TPad("overlay","",0.5, 0.3, 1, 1)
                                         overlay = ROOT.TPad("overlay","",0, 0.05, 1, 0.5)
                                         overlay.SetFillStyle(4000)
                                         overlay.SetFillColor(0)
@@ -740,7 +757,8 @@ for item in plots:
                                         elif bins[i] < 380: just_poly.GetXaxis().SetRangeUser(0, 20)
                                         just_poly.SetTitle("")
                                         just_poly.Draw("AI L")
-                                        if FTEST: tight_stat.Draw()
+                                        legend2.AddEntry('', 'Chi2/Ndof: {:.3f}'.format(chi2_ndof), '')
+                                        legend2.AddEntry('', 'Bin Error: {:.1%}'.format(bin_bin_error), '')
                                 
                                 if not args.fit:
                                     c1.cd()
