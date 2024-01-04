c1b29f8b96b1916b65549664015b7fded5907bb0
Brandon Chiarito <bchiarito@gmail.com>
2024-01-04 18:49:47 -0500

    move guesses to fitting_utils

--- unit_test.sh
+++ unit_test.sh
@@ -4,6 +4,7 @@ if [[ $# -ne 1 ]]; then
     exit 2
 fi
 rm -rf $1/scaled_tight_hists/
+rm -rf $1/scaled_phislice_tight_hists/
 rm -rf $1/loose_fit_hists/
 rm -rf $1/tight_templates/
 rm $1/test.pdf
@@ -12,7 +13,4 @@ python run_fitter.py $1 --testBin "noniso_sym barrel 100" --ftest "3 1" --useSca
 python run_fitter.py $1 --testBin "noniso_sym barrel 100" --ftest "3 1"
 python run_plotter.py $1 --testBin "noniso_sym barrel 100" --name "test" --useScaledTight
 rm $1/plots.pdf
-rm -rf $1/scaled_tight_hists/
-rm -rf $1/loose_fit_hists/
-rm -rf $1/tight_templates/
 xdg-open $1/test.pdf
